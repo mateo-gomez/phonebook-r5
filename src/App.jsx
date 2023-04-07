1159c626621bd863ca1386549515913961deec44
Mateo Gomez <mateogomezmolina@gmail.com>
2023-04-07 15:52:02 -0500

    Add handlers for modal and remove contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-import ContactList from './components/ContactList'
-import ContactForm from './components/ContactForm'
+import { useState } from "react";
+import "./App.css";
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
+import Modal from "./components/Modal";
+import Button from "./components/Button";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+	const [contacts, setContacts] = useState([]);
+	const [showContactForm, setShowContactForm] = useState(false);
 
-  const handleSubmit = (form) => {
-    form.preventDefault()
+	const handleSubmit = (form) => {
+		form.preventDefault();
 
-    const data = new FormData(form.target)
+		const data = new FormData(form.target);
 
-    const newContact = {
-      id: window.crypto.randomUUID(),
-      first_name: data.get('first_name'),
-      last_name: data.get('last_name'),
-      phone_number: data.get('phone_number'),
-    }
+		const newContact = {
+			id: window.crypto.randomUUID(),
+			first_name: data.get("first_name"),
+			last_name: data.get("last_name"),
+			phone_number: data.get("phone_number"),
+		};
 
-    addContact(newContact)
-  }
+		addContact(newContact);
+	};
 
-  const addContact = (contact) => {
-    setContacts((contacts) => [...contacts, contact])
-  }
+	const addContact = (contact) => {
+		setContacts((contacts) => [...contacts, contact]);
+		setShowContactForm(false);
+	};
 
-  return (
-    <main>
-      <h1>Phonebook</h1>
+	const handleRemoveContact = (id) => {
+		setContacts((contacts) => {
+			return contacts.filter((item) => item.id !== id);
+		});
+	};
+
+	const handleCloseForm = () => {
+		setShowContactForm(false);
+	};
+
+	return (
+		<main>
+			<h1>Phonebook</h1>
 
 			<Button onClick={() => setShowContactForm(true)}>Add contact</Button>
 			<Modal show={showContactForm} onClose={handleCloseForm}>
 				<ContactForm onSubmit={handleSubmit} onDismiss={handleCloseForm} />
 			</Modal>
 
-      <section>
-        <ContactList data={contacts} />
-      </section>
-    </main>
-  )
+			<ContactList data={contacts} onClickRemove={handleRemoveContact} />
+		</main>
+	);
 }
 
-export default App
+export default App;
