c48cbb7c90cb2bfbbcf75832c73af1458f9ca3f1
Mateo Gomez <mateogomezmolina@gmail.com>
2023-04-07 15:48:13 -0500

    Add remove contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,25 @@
-import Button from './Button'
+import Button from "./Button";
 
 const Contact = ({ contact, onClickRemove, onEdit }) => {
-  return (
-    <li key={contact.id} className='contact'>
-      <div className='contact-info'>
-        <p>
-          {contact.first_name} {contact.last_name}
-        </p>
-        <span>{contact.phone_number}</span>
-      </div>
+	const handleClickRemove = () => {
+		onClickRemove(contact.id);
+	};
 
-      <div className='contact-actions'>
-        <Button onClick={onClickRemove}>Remove</Button>
-        <Button onClick={onEdit}>Edit</Button>
-      </div>
-    </li>
-  )
-}
+	return (
+		<li key={contact.id} className="contact">
+			<div className="contact-info">
+				<p>
+					{contact.first_name} {contact.last_name}
+				</p>
+				<span>{contact.phone_number}</span>
+			</div>
 
-export default Contact
+			<div className="contact-actions">
+				<Button onClick={handleClickRemove}>Remove</Button>
+				<Button onClick={onEdit}>Edit</Button>
+			</div>
+		</li>
+	);
+};
+
+export default Contact;
