fd52bf99305705fa48478c368aa296d6a5126f8d
Mateo Gomez <mateogomezmolina@gmail.com>
2023-04-07 15:43:46 -0500

    Add "no contacts found text"

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,21 +1,25 @@
-import Card from './Card'
-import Contact from './Contact'
+import Card from "./Card";
+import Contact from "./Contact";
 
 const ContactList = ({ data, onClickEdit, onClickRemove }) => {
-  return (
-    <Card>
-      <ul>
-        {data.map((contact) => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            onClickEdit={onClickEdit}
-            onClickRemove={onClickRemove}
-          />
-        ))}
-      </ul>
-    </Card>
-  )
-}
+	return (
+		<Card>
+			<ul>
+				{data.length > 0 ? (
+					data.map((contact) => (
+						<Contact
+							key={contact.id}
+							contact={contact}
+							onClickEdit={onClickEdit}
+							onClickRemove={onClickRemove}
+						/>
+					))
+				) : (
+					<div style={{ textAlign: "center" }}>No contacts found</div>
+				)}
+			</ul>
+		</Card>
+	);
+};
 
-export default ContactList
+export default ContactList;
