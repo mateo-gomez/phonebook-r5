84b4a5f3460bb39d2ee7ec5011ca6643fa34c1e5
Mateo Gomez <mateogomezmolina@gmail.com>
2023-04-09 15:20:05 -0500

    Refactor search input

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,20 +1,12 @@
-import Button from "./Button";
-
 export const InputSearch = ({ onChange }) => {
 	return (
-		<div className="search-container">
-			<form action="" method="get">
-				<input
-					className="search-input"
-					type="search"
-					name="search"
-					placeholder="Search..."
-					onChange={onChange}
-				/>
-
-				<Button className="search-button">🔍</Button>
-			</form>
-		</div>
+		<input
+			className="search"
+			type="search"
+			name="search"
+			placeholder="Search..."
+			onChange={onChange}
+		/>
 	);
 };
 
